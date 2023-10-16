8dc1f0673f97207830beef4e2fa833b18efe69ab
Kibum Han <kibumh@gmail.com>
2023-10-16 16:57:02 +0900

    python.3: Implement step3_env

--- impls/python.3/mw.py
+++ impls/python.3/mw.py
@@ -1,5 +1,37 @@
-from typing import List, TypeAlias
+from typing import List, Optional, TypeAlias
 
 Symbol: TypeAlias = str
 
 Expr: TypeAlias = int | Symbol | List["Expr"]
+
+
+###############
+# Environment #
+###############
+
+
+class EnvError(Exception):
+    pass
+
+
+class Env:
+    def __init__(self, outer: Optional["Env"] = None):
+        self._outer = outer
+        self._data = {}
+
+    def set(self, key: Symbol, value: Expr) -> Expr:
+        self._data[key] = value
+        return value
+
+    def find(self, key: Symbol) -> Optional["Env"]:
+        if key in self._data:
+            return self
+        if self._outer is None:
+            return None
+        return self._outer.find(key)
+
+    def get(self, key: Symbol) -> Expr:
+        d = self.find(key)
+        if d is None:
+            raise EnvError(f"'{key}' not found")
+        return d._data[key]
---
+++ impls/python.3/step3_env.py
@@ -0,0 +1,63 @@
+import mw
+import printer
+import reader
+
+_PROMPT = "user> "
+
+
+def eval_ast(e: mw.Expr, env) -> mw.Expr:
+    if isinstance(e, mw.Symbol):
+        return env.get(e)
+    if isinstance(e, list):
+        return [EVAL(child, env) for child in e]
+    return e
+
+
+def READ(s: str) -> str:
+    return reader.read_str(s)
+
+
+def EVAL(e: mw.Expr, env) -> mw.Expr:
+    if not isinstance(e, list):
+        return eval_ast(e, env)
+    if len(e) == 0:
+        return e
+    match e[0]:
+        case "def!":
+            return env.set(e[1], EVAL(e[2], env))
+        case "let*":
+            new_env = mw.Env(env)
+            for k, v in zip(e[1][::2], e[1][1::2]):
+                new_env.set(k, EVAL(v, new_env))  # env or new_env?
+            return EVAL(e[2], new_env)
+        case default:
+            e = eval_ast(e, env)
+            return e[0](*e[1:])
+
+
+def PRINT(e: mw.Expr) -> str:
+    return printer.pr_str(e)
+
+
+def rep(arg: str, env) -> None:
+    return PRINT(EVAL(READ(arg), env))
+
+
+def main() -> None:
+    env = mw.Env()
+    env.set(mw.Symbol("+"), lambda a, b: a + b)
+    env.set(mw.Symbol("-"), lambda a, b: a - b)
+    env.set(mw.Symbol("*"), lambda a, b: a * b)
+    env.set(mw.Symbol("/"), lambda a, b: a // b)
+    while True:
+        print(_PROMPT, end="")
+        try:
+            print(rep(input(), env))
+        except reader.SyntaxError as e:
+            print(e)
+        except mw.EnvError as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
